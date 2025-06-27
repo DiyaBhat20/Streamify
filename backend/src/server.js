dedbbbb6d183b7ca13fb6d7580a0eb47038cf358
Diya Bhat <diyabhat427@gmail.com>
2025-06-28 00:11:58 +0530

    Chat Routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();

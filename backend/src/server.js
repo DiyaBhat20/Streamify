2978bb35897bea29b468a94424230f0c4a05b728
Diya Bhat <diyabhat427@gmail.com>
2025-06-27 23:04:02 +0530

    Get Recommended Users & Friends Endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -10,7 +11,8 @@ const PORT=process.env.PORT
 
 app.use(express.json());
 
-app.use("/api/auth",authRoutes)
+app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
 
 app.listen(PORT,()=>{
   console.log(`Server running on Port : ${PORT}`);
